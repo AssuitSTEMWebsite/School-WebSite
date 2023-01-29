eb6e65798b45abd8129068fe08a632cc72407bc2
BeboElRashidy <80958450+BeboElRashidy@users.noreply.github.com>
2023-01-29 12:52:40 +0300

    Add files via upload

---
+++ alumni.js
@@ -0,0 +1,252 @@
+  var s1 = 0;
+  var s2 = 0;
+  var s3 = 0;
+  var s4 = 0;
+  var s5 = 0;
+  var s6 = 0;
+  var s7 = 0;
+  var s8 = 0;
+  var s9 = 0;
+  var s10 = 0;
+  var s11 = 0;
+  var s12 = 0;
+  var s13 = 0;
+  var s14 = 0;
+  var s15 = 0;
+  var s16 = 0;
+  function hot1() {
+    if (s1 == 0) {
+      s1= 1;
+    document.getElementById('firstt').innerHTML ='<div class="topright"></div> <div class="bottomleft"></div><section id="first" style="color: black; font-weight: bold; position: relative; top:120px ;"><h5>Youssef Ashraf Naeem</h5><h5>David Osama</h5></section>';
+    document.getElementById('firstt').setAttribute("class", "rotate");
+    
+  }
+  else{
+    s1 =0;
+    document.getElementById('firstt').setAttribute("class", "rotate2");
+    document.getElementById('firstt').innerHTML=' <div class="topright"></div> <div class="bottomleft"></div><section  id="first"><img src="logos/Minerva.png" ><h3 style="color: black; font-weight: bold; position: relative; top:120px ;"> Minerva </h3></section>';
+
+  }
+    
+  }
+  function hot2() {
+    if (s2 == 0) {
+      s2= 1;
+    document.getElementById('secondd').innerHTML ='<div class="topright"></div> <div class="bottomleft"></div><section id="first" style="color: black; font-weight: bold; position: relative; top:120px ;"><h5>Ahmed Yasser</h5><h5>Khaled Mamdouh</h5><h5>Ahmed Amr</h5><h5>Abdallah Hasan</h5></section>';
+    document.getElementById('secondd').setAttribute("class", "rotate");
+    
+  }
+  else{
+    s2 =0;
+    document.getElementById('secondd').setAttribute("class", "rotate2");
+    document.getElementById('secondd').innerHTML=' <div class="topright"></div><div class="bottomleft"></div><section id="second"><img src="logos/auc.png"><h3 style="color: black; font-weight: bold; position: relative; top:120px ;"> America University In Cairo </h3></section>';
+
+  }
+    
+  }
+function hot3() {
+    if (s3 == 0) {
+      s3= 1;
+    document.getElementById('thirdd').innerHTML ='<div class="topright"></div> <div class="bottomleft"></div><section id="first" style="color: black; font-weight: bold; position:relative;"> <h5>Mostafa Ahmed AlGaafary</h5><h5>Ahmed Nasr</h5><h5>Ahmed Fathi</h5><h5>Mohamed ElFateh shaaban</h5><h5>Youssef Ahmed Hozien</h5><h5>Abdelrahman Ahmed Omar</h5><h5>Tameem Alaa</h5></section>';
+    document.getElementById('thirdd').setAttribute("class", "rotate");
+    
+  }
+  else{
+    s3 =0;
+    document.getElementById('thirdd').setAttribute("class", "rotate2");
+    document.getElementById('thirdd').innerHTML=' <div class="topright"></div><div class="bottomleft"></div><section id="third-first"><img src="logos/E-JUST.png"><h3 style="color: black; font-weight: bold; position: relative; top:40px ;"> Egyption-Japanese Science and Technology University </h3></section>';
+
+  }
+    
+  }
+  function hot4() {
+    if (s4 == 0) {
+      s4= 1;
+    document.getElementById('fourthh').innerHTML ='<div class="topright"></div> <div class="bottomleft"></div><section id="first" style="color: black; font-weight: bold; position: relative; top:120px ;" ><h5>Mohamed Osama Rashad</h5></section>';
+    document.getElementById('fourthh').setAttribute("class", "rotate");
+    
+  }
+  else{
+    s4 =0;
+    document.getElementById('fourthh').setAttribute("class", "rotate2");
+    document.getElementById('fourthh').innerHTML=' <div class="topright"></div> <div class="bottomleft"></div><section id="third" ><img src="logos/dobrecon.png"><h3 style="color: black; font-weight: bold; position: relative; top:120px ;"> University of Debrecen </h3> </section>';
+
+  }
+    
+  }
+  function hot5() {
+    if (s5 == 0) {
+      s5= 1;
+    document.getElementById('fifthh').innerHTML ='<div class="topright"></div> <div class="bottomleft"></div><section id="first" ><h3 style="color: black; font-weight: bold; position: relative; top:120px ;"> Youssef Ashraf Naeem </h3></section>';
+    document.getElementById('fifthh').setAttribute("class", "rotate");
+    
+  }
+  else{
+    s5 =0;
+    document.getElementById('fifthh').setAttribute("class", "rotate2");
+    document.getElementById('fifthh').innerHTML=' <div class="topright"></div> <div class="bottomleft"></div><section id="fourth"><img src="logos/ConCordia.png" > <h3 style="color: black; font-weight: bold; position: relative; top:40px ;"> Concordia University Canada </h3></section>';
+
+  }
+    
+  }
+  function hot6() {
+    if (s6 == 0) {
+      s6= 1;
+    document.getElementById('sixthh').innerHTML ='<div class="topright"></div> <div class="bottomleft"></div><section id="first"  style="position:relative; color: black; font-weight: bold;"><h5>George Emad Kamal</h5></section>';
+    document.getElementById('sixthh').setAttribute("class", "rotate");
+    
+  }
+  else{
+    s6 =0;
+    document.getElementById('sixthh').setAttribute("class", "rotate2");
+    document.getElementById('sixthh').innerHTML=' <div class="topright"></div> <div class="bottomleft"></div><section id="fifth" ><img src="logos/know.jpg"><h3 style="position: relative; top:120px ;"> The Knoledge Hub - Coventry University </h3> </section>';
+
+  }
+    
+  }
+  function hot7() {
+    if (s7 == 0) {
+      s7= 1;
+    document.getElementById('seventhh').innerHTML ='<div class="topright"></div> <div class="bottomleft"></div><section id="first" style=" position:relative; display:flex; justify-content:center; align-items:center; color: black; font-weight: bold;"><h5>Jessica Magdy</h5></section>';
+    document.getElementById('seventhh').setAttribute("class", "rotate");
+    
+  }
+  else{
+    s7 =0;
+    document.getElementById('seventhh').setAttribute("class", "rotate2");
+    document.getElementById('seventhh').innerHTML=' <div class="topright"></div> <div class="bottomleft"></div><section id="sixth"><img src="logos/GUC.png"><h3 style="color: black; font-weight: bold;position: relative; top:120px ;"> German University in Cairo</h3></section>';
+
+  }
+    
+  }
+  function hot8() {
+    if (s8 == 0) {
+      s8= 1;
+    document.getElementById('eightt').innerHTML ='<div class="topright"></div> <div class="bottomleft"></div><section id="first" style=" position:relative; display:flex; justify-content:center; align-items:center; color: black; font-weight: bold;" ><h5>Abdelatief Khaled Abdelatief</h5><h5>Ahmed Safwat</h5><h5>Jomana Ayman</h5><h5>Wagdy Adel</h5></section>';
+    document.getElementById('eightt').setAttribute("class", "rotate");
+    
+  }
+  else{
+    s8 =0;
+    document.getElementById('eightt').setAttribute("class", "rotate2");
+    document.getElementById('eightt').innerHTML=' <div class="topright"></div> <div class="bottomleft"></div><section id="seventh" ><img src="logos/zewail.png"> <h3 style="color: black; font-weight: bold; position: relative; top:120px ;">Zewail City of Science and Technology</h3></section>';
+
+  }
+    
+  }
+  function hot9() {
+    if (s9 == 0) {
+      s9= 1;
+    document.getElementById('ninthh').innerHTML ='<div class="topright"></div> <div class="bottomleft"></div><section id="first" style=" position:relative; display:flex; justify-content:center; align-items:center; color: black; font-weight: bold;"><h5>Adham Remeh</h5><h5>Hossam ElDen Ahmed Heleza</h5><h5>Hasnaa Gamal</h3><h5>Fady Nagy</h5></section>';
+    document.getElementById('ninthh').setAttribute("class", "rotate");
+    
+  }
+  else{
+    s9 =0;
+    document.getElementById('ninthh').setAttribute("class", "rotate2");
+    document.getElementById('ninthh').innerHTML=' <div class="topright"></div> <div class="bottomleft"></div><section id="eight"><img src="logos/NU.png"><h3 style="color:black; font-weight: bold; position: relative; top:150px ;"> Nile University </h3></section>';
+
+  }
+    
+  }
+  function hot10() {
+    if (s10 == 0) {
+      s10= 1;
+    document.getElementById('tenthh').innerHTML ='<div class="topright"></div> <div class="bottomleft"></div><section id="first" style=" position:relative; display:flex; justify-content:center; align-items:center; color: black; font-weight: bold;" ><h5>Abdallah Monzer</h5></section>';
+    document.getElementById('tenthh').setAttribute("class", "rotate");
+    
+  }
+  else{
+    s10 =0;
+    document.getElementById('tenthh').setAttribute("class", "rotate2");
+    document.getElementById('tenthh').innerHTML=' <div class="topright"></div> <div class="bottomleft"></div><section id="tenth" ><img src="logos/British.png"> <h3 style="position: relative; top:120px ; font-weight:bold;">British University in Egypt</h3></section></div>';
+
+  }
+    
+  }
+  function hot11() {
+    if (s11 == 0) {
+      s11= 1;
+    document.getElementById('eleventhh').innerHTML ='<div class="topright"></div> <div class="bottomleft"></div><section id="first" ><h5>Ahmed Mohamed AboDeif</h5></section>';
+    document.getElementById('eleventhh').setAttribute("class", "rotate");
+    
+  }
+  else{
+    s11 =0;
+    document.getElementById('eleventhh').setAttribute("class", "rotate2");
+    document.getElementById('eleventhh').innerHTML=' <div class="topright"></div> <div class="bottomleft"></div><section id="eleventh" ><img src="logos/Canadian.png"> <h3 style="position: relative; top:100px ; font-weight:bold;">University of Canada in Egypt</h3></section></div>';
+
+  }
+    
+  }
+  function hot12() {
+    if (s12 == 0) {
+      s12= 1;
+    document.getElementById('twelvethh').innerHTML ='<div class="topright"></div> <div class="bottomleft"></div><section id="first" ><h5>Ahmed Mohamed ElShazly</h5></section>';
+    document.getElementById('twelvethh').setAttribute("class", "rotate");
+    
+  }
+  else{
+    s12 =0;
+    document.getElementById('twelvethh').setAttribute("class", "rotate2");
+    document.getElementById('twelvethh').innerHTML=' <div class="topright"></div> <div class="bottomleft"></div><section id="twelveth" ><img src="logos/ALU.png"> <h3 style="position: relative; top:200px ; font-weight:bold;">African Leadership University</h3></section></div>';
+
+  }
+    
+  }
+  function hot13() {
+    if (s13 == 0) {
+      s13= 1;
+    document.getElementById('thirteenn').innerHTML ='<div class="topright"></div> <div class="bottomleft"></div><section id="first" ><h5>Mohamed Hamdy</h5></section>';
+    document.getElementById('thirteenn').setAttribute("class", "rotate");
+    
+  }
+  else{
+    s13 =0;
+    document.getElementById('thirteenn').setAttribute("class", "rotate2");
+    document.getElementById('thirteenn').innerHTML=' <div class="topright"></div> <div class="bottomleft"></div><section id="thirteen" ><img src="logos/Innopolis.jpg"> <h3 style="position: relative; top:120px ; font-weight:bold;">Innopolis University</h3></section></div>';
+  }
+    
+  }
+  function hot14() {
+    if (s14 == 0) {
+      s14= 1;
+    document.getElementById('fourteenn').innerHTML ='<div class="topright"></div> <div class="bottomleft"></div><section id="first" ><h5>Ahmed Mamdouh ElNoamany</h5></section>';
+    document.getElementById('fourteenn').setAttribute("class", "rotate");
+    
+  }
+  else{
+    s14 =0;
+    document.getElementById('fourteenn').setAttribute("class", "rotate2");
+    document.getElementById('fourteenn').innerHTML='<div class="topright"></div> <div class="bottomleft"></div><section id="fourteen" ><img src="logos/AIU.png"> <h3 style="position: relative; top:90px ; font-weight:bold;">AlAlamein Internaional University</h3></section></div>';
+  }
+    
+  }
+  function hot15() {
+    if (s15 == 0) {
+      s15= 1;
+    document.getElementById('fifteenn').innerHTML ='<div class="topright"></div> <div class="bottomleft"></div><section id="first" ><h5>Ahmed Mohamed Tawfiq</h5></section>';
+    document.getElementById('fifteenn').setAttribute("class", "rotate");
+    
+  }
+  else{
+    s15 =0;
+    document.getElementById('fifteenn').setAttribute("class", "rotate2");
+    document.getElementById('fifteenn').innerHTML=' <div class="topright"></div> <div class="bottomleft"></div><section id="fifteen" ><img src="logos/MSA.png"> <h3 style="position: relative; top:120px ; font-weight:bold;"> October University for Modern Science & Arts </h3></section></div>';
+  }
+    
+  }
+  function hot16() {
+    if (s16 == 0) {
+      s16= 1;
+    document.getElementById('sixteenn').innerHTML ='<div class="topright"></div> <div class="bottomleft"></div><section id="first" ><h5>Sharlz Nabil</h5></section>';
+    document.getElementById('sixteenn').setAttribute("class", "rotate");
+    
+  }
+  else{
+    s16 =0;
+    document.getElementById('sixteenn').setAttribute("class", "rotate2");
+    document.getElementById('sixteenn').innerHTML=' <div class="topright"></div> <div class="bottomleft"></div><section id="sixteen" ><img src="logos/MUST.png"> <h3 style="position: relative; top:120px ; font-weight:bold;"> Misr University for Science & Technology </h3></section></div>';
+  }
+    
+  }
